1cdea167b6d694e6e748f5a8a702d0f6cfc9f98c
Wlademir Renan dos Santos <wlademir.renan@gmail.com>
2016-03-30 21:16:55 -0300

    Ajustando actions para o necessario do desafio

--- tqi/app/controllers/pedidos_controller.rb
+++ tqi/app/controllers/pedidos_controller.rb
@@ -1,74 +1,19 @@
 class PedidosController < ApplicationController
-  before_action :set_pedido, only: [:show, :edit, :update, :destroy]
 
-  # GET /pedidos
-  # GET /pedidos.json
   def index
-    @pedidos = Pedido.all
-  end
-
-  # GET /pedidos/1
-  # GET /pedidos/1.json
-  def show
-  end
-
-  # GET /pedidos/new
-  def new
     @pedido = Pedido.new
   end
-
-  # GET /pedidos/1/edit
-  def edit
-  end
-
-  # POST /pedidos
-  # POST /pedidos.json
-  def create
-    @pedido = Pedido.new(pedido_params)
-
-    respond_to do |format|
-      if @pedido.save
-        format.html { redirect_to @pedido, notice: 'Pedido was successfully created.' }
-        format.json { render :show, status: :created, location: @pedido }
-      else
-        format.html { render :new }
-        format.json { render json: @pedido.errors, status: :unprocessable_entity }
-      end
+  
+  def index_do
+    unless params[:pedido].nil?
+      # Metodo de classe para gravar conteudo do formulário
+      @numero_pedidos_importados = Pedido.inserir_pedidos(params[:pedido])
+      @pedidos = Pedido.order("created_at DESC").limit(@numero_pedidos_importados)
+      @valor_total_pedidos = @pedidos.inject(0){|total, pedido| total = total + pedido.valor_total}
+    else
+      @pedido = Pedido.new
+      flash[:notice] = "Insira algum arquivo de pedidos"
+      render :action => 'index'
     end
   end
-
-  # PATCH/PUT /pedidos/1
-  # PATCH/PUT /pedidos/1.json
-  def update
-    respond_to do |format|
-      if @pedido.update(pedido_params)
-        format.html { redirect_to @pedido, notice: 'Pedido was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pedido }
-      else
-        format.html { render :edit }
-        format.json { render json: @pedido.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /pedidos/1
-  # DELETE /pedidos/1.json
-  def destroy
-    @pedido.destroy
-    respond_to do |format|
-      format.html { redirect_to pedidos_url, notice: 'Pedido was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pedido
-      @pedido = Pedido.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def pedido_params
-      params.require(:pedido).permit(:comprador, :descricao, :preco_unitario, :quantidade, :endereco, :fornecedor, :valor_total)
-    end
 end
